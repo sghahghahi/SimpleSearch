bbe0ae393bffd927eb834b1c7c53e35ef9704509
sghahghahi <shyon.ghahghahi@gmail.com>
2024-11-11 01:15:49 -0800

    Add methods indexLocation(), indexDirectory(), indexFile(), and isTextFile(), and add static nested class Work

--- src/main/java/edu/usfca/cs272/ThreadSafeTextFileIndexer.java
+++ src/main/java/edu/usfca/cs272/ThreadSafeTextFileIndexer.java
@@ -1,27 +1,23 @@
 package edu.usfca.cs272;
 
-import static opennlp.tools.stemmer.snowball.SnowballStemmer.ALGORITHM.ENGLISH;
+import java.io.BufferedReader;
+import java.io.IOException;
 
-import edu.usfca.cs272.MultiReaderLock.SimpleLock;
+import static java.nio.charset.StandardCharsets.UTF_8;
+import java.nio.file.DirectoryStream;
+import java.nio.file.Files;
+import java.nio.file.Path;
+
 import opennlp.tools.stemmer.snowball.SnowballStemmer;
+import static opennlp.tools.stemmer.snowball.SnowballStemmer.ALGORITHM.ENGLISH;
 
 /** TODO */
 public class ThreadSafeTextFileIndexer {
-	/** TOdO */
+	/** TODO */
 	private final InvertedIndex invertedIndex;
 
 	/** TODO */
-	// Simliar to search, give each thread a stemmer?
 	private final SnowballStemmer snowballStemmer;
-
-	/** TODO */
-	private final MultiReaderLock lock;
-
-	/** TODO */
-	private final SimpleLock readLock;
-
-	/** TODO */
-	private final SimpleLock writeLock;
 
 	/** TODO */
 	private final WorkQueue queue;
@@ -31,12 +27,112 @@ public class ThreadSafeTextFileIndexer {
 	 * @param invertedIndex
 	 * @param numThreads
 	 */
-	public ThreadSafeTextFileIndexer(InvertedIndex invertedIndex, int numThreads) {
+	public ThreadSafeTextFileIndexer(InvertedIndex invertedIndex, WorkQueue queue) {
 		this.invertedIndex = invertedIndex;
 		this.snowballStemmer = new SnowballStemmer(ENGLISH);
-		this.lock = new MultiReaderLock();
-		this.readLock = this.lock.readLock();
-		this.writeLock = this.lock.writeLock();
-		this.queue = new WorkQueue(numThreads);
+		this.queue = queue;
+	}
+
+	private static class Work implements Runnable {
+		/** TODO */
+		Path location;
+
+		/** TODO */
+		ThreadSafeTextFileIndexer indexer;
+
+		/**
+		 * TODO
+		 * @param location
+		 * @param threadSafeTextFileIndexer
+		 */
+		public Work(Path location, ThreadSafeTextFileIndexer indexer) {
+			this.location = location;
+			this.indexer = indexer;
+		}
+
+		/** TODO */
+		@Override
+		public void run() {
+			try {
+				this.indexer.indexFile(location);
+			} catch (IOException e) {
+				System.err.println("IOException occured during run() method.");
+				Thread.currentThread().interrupt();
+			}
+		}
+
+	}
+
+	/**
+	 * TODO
+	 * @param path
+	 * @throws IOException
+	 */
+	public void indexFile(Path path) throws IOException {
+		try (BufferedReader reader = Files.newBufferedReader(path, UTF_8)) {
+			int wordPosition = 1;
+			String line = null;
+			String location = path.toString();
+
+			while ((line = reader.readLine()) != null) {
+				String[] cleanedWords = FileStemmer.parse(line);
+
+				for (String cleanWord : cleanedWords) {
+					String stemmedWord;
+					synchronized (this.snowballStemmer) {
+						stemmedWord = this.snowballStemmer.stem(cleanWord).toString();
+					}
+					synchronized (this.invertedIndex) {
+						this.invertedIndex.addWordPosition(stemmedWord, location, wordPosition++);
+					}
+				}
+			}
+		}
+	}
+
+	/**
+	 * TODO
+	 * @param dirLocation
+	 * @throws IOException
+	 */
+	public void indexDirectory(Path dirLocation) throws IOException {
+		try (DirectoryStream<Path> dirStream = Files.newDirectoryStream(dirLocation)) {
+			for (Path location : dirStream) {
+				if (Files.isDirectory(location)) {
+					indexDirectory(location);
+				} else if (isTextFile(location)) {
+					Work work = new Work(location, this);
+					this.queue.execute(work);
+				}
+			}
+		}
+	}
+
+	/**
+	 * TODO
+	 * @param location
+	 * @return
+	 */
+	public static boolean isTextFile(Path location) {
+		String lowerCaseLocation = location.toString().toLowerCase();
+
+		return (
+			lowerCaseLocation.endsWith(".txt") ||
+			lowerCaseLocation.endsWith(".text")
+		);
+	}
+
+	/**
+	 * TODO
+	 * @param location
+	 * @throws IOException
+	 */
+	public void indexLocation(Path location) throws IOException {
+		if (Files.isDirectory(location)) {
+			indexDirectory(location);
+		} else {
+			Work work = new Work(location, this);
+			this.queue.execute(work);
+		}
 	}
 }
