02e7514c6fbf3edaca4a739946c2572f7dbc0b61
sghahghahi <shyon.ghahghahi@gmail.com>
2024-12-03 03:31:47 -0800

    Add Javadoc comments to WebCrawler.java

--- src/main/java/edu/usfca/cs272/WebCrawler.java
+++ src/main/java/edu/usfca/cs272/WebCrawler.java
@@ -7,24 +7,32 @@ import java.util.ArrayList;
 
 import opennlp.tools.stemmer.snowball.SnowballStemmer;
 
-/** TODO */
+/**
+ * Class responsible for web crawling starting from a specific seed URI.
+ * Will only redirect up to three times.
+ * Builds an inverted index from the seed URI.
+ *
+ * @author Shyon Ghahghahi
+ * @author CS 272 Software Development (University of San Francisco)
+ * @version Fall 2024
+ */
 public class WebCrawler {
-	/** TODO  */
+	/** The inverted index to add to */
 	private final ThreadSafeInvertedIndex invertedIndex;
 
-	/** TODO */
+	/** The initial {@code URI} to build an inverted index from */
 	private final URI seedURI;
 
-	/** TODO */
+	/** The maximum number of redirects allowed */
 	private static final int MAX_REDIRECTS = 3;
 
-	/** TODO */
+	/** The stemmer to use class-wide */
 	private final SnowballStemmer snowballStemmer;
 
 	/**
-	 * TODO
-	 * @param invertedIndex
-	 * @param seedURI
+	 * Constructs a web cralwer with a thread-safe inverted index and seed URI
+	 * @param invertedIndex The inverted index to add to
+	 * @param seedURI The seed URI to build the inverted index from
 	 */
 	public WebCrawler(ThreadSafeInvertedIndex invertedIndex, URI seedURI) {
 		this.invertedIndex = invertedIndex;
@@ -33,7 +41,9 @@ public class WebCrawler {
 	}
 
 	/**
-	 * TODO
+	 * Starts crawling from the seed URI.
+	 * Will redirect up to three times.
+	 * Adds words and their locations to the inverted index
 	 */
 	public void crawl() {
 		String html = HtmlFetcher.fetch(this.seedURI, MAX_REDIRECTS);
