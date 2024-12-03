53f38ba74898becaa51ba6d57b5425543039198e
sghahghahi <shyon.ghahghahi@gmail.com>
2024-12-02 17:13:08 -0800

    Add constructor and members to WebCrawler.java

--- src/main/java/edu/usfca/cs272/WebCrawler.java
+++ src/main/java/edu/usfca/cs272/WebCrawler.java
@@ -1,3 +1,27 @@
 package edu.usfca.cs272;
 
-public class WebCrawler {}
+import edu.usfca.cs272.HttpsFetcher;
+import edu.usfca.cs272.HtmlFetcher;
+import edu.usfca.cs272.HtmlCleaner;
+
+
+import java.io.IOException;
+import java.net.URISyntaxException;
+
+/** TODO */
+public class WebCrawler {
+	/** TODO  */
+	private final ThreadSafeInvertedIndex invertedIndex;
+
+	/** TODO */
+	private final String seedURI;
+
+	/**
+	 * TODO
+	 * @param invertedIndex
+	 */
+	public WebCrawler(ThreadSafeInvertedIndex invertedIndex, String seedURI) throws URISyntaxException, IOException {
+		this.invertedIndex = invertedIndex;
+		this.seedURI = seedURI;
+	}
+}
