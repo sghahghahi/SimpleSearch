c275d13f0c82618fc9ff2f441595014a207a8264
sghahghahi <shyon.ghahghahi@gmail.com>
2024-12-03 02:46:04 -0800

    Add more members and implement crawl()

--- src/main/java/edu/usfca/cs272/WebCrawler.java
+++ src/main/java/edu/usfca/cs272/WebCrawler.java
@@ -1,27 +1,52 @@
 package edu.usfca.cs272;
 
-import edu.usfca.cs272.HttpsFetcher;
-import edu.usfca.cs272.HtmlFetcher;
-import edu.usfca.cs272.HtmlCleaner;
+import static opennlp.tools.stemmer.snowball.SnowballStemmer.ALGORITHM.ENGLISH;
 
+import java.net.URI;
+import java.util.ArrayList;
 
-import java.io.IOException;
-import java.net.URISyntaxException;
+import opennlp.tools.stemmer.snowball.SnowballStemmer;
 
-/** TODO */
+/** TODO. Mention that this class is thread-safe */
 public class WebCrawler {
 	/** TODO  */
 	private final ThreadSafeInvertedIndex invertedIndex;
 
 	/** TODO */
-	private final String seedURI;
+	private final URI seedURI;
+
+	/** TODO */
+	private static final int MAX_REDIRECTS = 3;
+
+	/** TODO */
+	private final SnowballStemmer snowballStemmer;
 
 	/**
 	 * TODO
 	 * @param invertedIndex
+	 * @param seedURI
 	 */
-	public WebCrawler(ThreadSafeInvertedIndex invertedIndex, String seedURI) throws URISyntaxException, IOException {
+	public WebCrawler(ThreadSafeInvertedIndex invertedIndex, URI seedURI) {
 		this.invertedIndex = invertedIndex;
 		this.seedURI = seedURI;
+		this.snowballStemmer = new SnowballStemmer(ENGLISH);
+	}
+
+	/**
+	 * TODO
+	 */
+	public void crawl() {
+		String html = HtmlFetcher.fetch(this.seedURI, MAX_REDIRECTS);
+		if (html == null) {
+			System.out.printf("Could not start crawl at %s\n", this.seedURI);
+			return;
+		}
+
+		html = HtmlCleaner.stripBlockElements(html);
+		String cleanedHtml = HtmlCleaner.stripTags(html);
+		cleanedHtml = HtmlCleaner.stripEntities(cleanedHtml);
+
+		ArrayList<String> stems = FileStemmer.listStems(cleanedHtml, this.snowballStemmer);
+		this.invertedIndex.addWords(stems, this.seedURI.toString(), 1);
 	}
 }
