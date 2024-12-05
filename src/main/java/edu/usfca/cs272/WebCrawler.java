ee937ee38bb92938e158c549a06740f05fd8bb45
sghahghahi <shyon.ghahghahi@gmail.com>
2024-12-05 01:58:21 -0800

    Add initial URI to Set in Work constructor, ensure we've crawled up to the maximum number of times, and add data to local index

--- src/main/java/edu/usfca/cs272/WebCrawler.java
+++ src/main/java/edu/usfca/cs272/WebCrawler.java
@@ -11,7 +11,7 @@ import static opennlp.tools.stemmer.snowball.SnowballStemmer.ALGORITHM.ENGLISH;
  * Class responsible for web crawling starting from a specific seed URI.
  * Will only redirect up to three times.
  * Builds an inverted index from the seed URI.
- * This class is thread-safe.
+ * This class is thread safe.
  *
  * @author Shyon Ghahghahi
  * @author CS 272 Software Development (University of San Francisco)
@@ -61,15 +61,14 @@ public class WebCrawler {
 		 */
 		public Work(URI link) {
 			this.link = link;
+			synchronized (crawledLinks) {
+				crawledLinks.add(this.link);
+			}
 		}
 
 		@Override
 		public void run() {
-			String html = null;
-			// Check if we've hit the redirect limit
-			if (crawledLinks.size() < MAX_REDIRECTS) {
-				html = HtmlFetcher.fetch(this.link, MAX_REDIRECTS);
-			}
+			String html = HtmlFetcher.fetch(this.link, MAX_REDIRECTS);
 
 			if (html == null) {
 				System.err.printf("Could not start crawl at %s\n", this.link);
@@ -79,16 +78,15 @@ public class WebCrawler {
 			html = HtmlCleaner.stripBlockElements(html);
 			ArrayList<URI> hyperlinks = LinkFinder.listUris(link, html);
 
-			for (URI hyperlink : hyperlinks) {
-				URI absoluteURI = null;
-				synchronized (seedURI) {
-					absoluteURI = LinkFinder.toAbsolute(seedURI, hyperlink.toString());
-				}
+			synchronized (crawledLinks) {
+				for (URI hyperLink : hyperlinks) {
+					if (crawledLinks.size() >= numCrawls) {
+						break;
+					}
 
-				synchronized (crawledLinks) {
-					if (absoluteURI != null && !crawledLinks.contains(absoluteURI)) {
-						crawledLinks.add(absoluteURI);
-						queue.execute(new Work(absoluteURI));
+					if (!crawledLinks.contains(hyperLink)) {
+						crawledLinks.add(hyperLink);
+						queue.execute(new Work(hyperLink));
 					}
 				}
 			}
@@ -98,8 +96,17 @@ public class WebCrawler {
 
 			SnowballStemmer snowballStemmer = new SnowballStemmer(ENGLISH);
 			ArrayList<String> stems = FileStemmer.listStems(cleanedHtml, snowballStemmer);
-			String cleanedURI = LinkFinder.clean(this.link).toString();
-			invertedIndex.addWords(stems, cleanedURI, 1);
+			URI absoulteURI = LinkFinder.toAbsolute(seedURI, this.link.toString());
+			if (absoulteURI == null) {
+				System.err.println("Could not make URI absoulte");
+				return;
+			}
+
+			InvertedIndex localIndex = new InvertedIndex();
+			localIndex.addWords(stems, absoulteURI.toString(), 1);
+
+			// No need to synchronize because invertedIndex is thread safe
+			invertedIndex.addAll(localIndex);
 		}
 	}
 
@@ -109,7 +116,6 @@ public class WebCrawler {
 	 * Adds words and their locations to the inverted index.
 	 */
 	public void crawl() {
-
 		this.queue.execute(new Work(this.seedURI));
 		this.queue.finish();
 	}
