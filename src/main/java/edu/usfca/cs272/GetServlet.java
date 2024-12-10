e0df8878a0c3659a55b1bb51324b3c9cba92797b
sghahghahi <shyon.ghahghahi@gmail.com>
2024-12-10 00:37:19 -0800

    Add scaffolding for basic web server and home page

---
+++ src/main/java/edu/usfca/cs272/GetServlet.java
@@ -0,0 +1,21 @@
+package edu.usfca.cs272;
+
+import java.io.IOException;
+import static java.nio.charset.StandardCharsets.UTF_8;
+import java.nio.file.Files;
+
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+/** TODO */
+public class GetServlet extends HttpServlet {
+	@Override
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+		String html = Files.readString(SearchEngine.template.resolve("index.html"), UTF_8);
+
+		response.setContentType("text/html");
+		response.setStatus(HttpServletResponse.SC_OK);
+		response.getWriter().println(html);
+	}
+}
--- src/main/java/edu/usfca/cs272/SearchEngine.java
+++ src/main/java/edu/usfca/cs272/SearchEngine.java
@@ -1,4 +1,39 @@
 package edu.usfca.cs272;
 
+import java.nio.file.Path;
+
+import org.eclipse.jetty.ee10.servlet.ServletContextHandler;
+import org.eclipse.jetty.ee10.servlet.ServletHolder;
+import org.eclipse.jetty.server.Server;
+
+
 /** TODO */
-public class SearchEngine {}
+public class SearchEngine {
+	/** TODO */
+	private final int port;
+
+	/** TODO */
+	public static final Path template = Path.of("project-sghahghahi", "src", "main", "resources");
+
+	/**
+	 * TODO
+	 * @param invertedIndex TODO
+	 * @param port TODO
+	 */
+	public SearchEngine(int port) {
+		this.port = port;
+	}
+
+	/** TODO */
+	public void launchServer() throws Exception {
+		Server server = new Server(this.port);
+
+		ServletContextHandler handler = new ServletContextHandler();
+		handler.addServlet(new ServletHolder(new GetServlet()), "/");
+		server.setHandler(handler);
+
+		server.start();
+		System.err.printf("Server started at %s:%d\n", "localhost", this.port);
+		server.join();
+	}
+}
