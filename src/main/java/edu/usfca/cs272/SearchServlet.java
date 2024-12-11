a392d7276d989c672393e96655e97153dda03360
sghahghahi <shyon.ghahghahi@gmail.com>
2024-12-11 01:20:26 -0800

    Override doGet() in SearchServlet to handle and process all form data

--- src/main/java/edu/usfca/cs272/SearchServlet.java
+++ src/main/java/edu/usfca/cs272/SearchServlet.java
@@ -1,7 +1,17 @@
 package edu.usfca.cs272;
 
+import java.io.IOException;
+import java.util.List;
+import java.util.TreeSet;
+
+import org.apache.commons.text.StringEscapeUtils;
+
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
 /** TODO */
-public class SearchServlet {
+public class SearchServlet extends HttpServlet {
 	/** TODO */
 	private final ThreadSafeInvertedIndex invertedIndex;
 
@@ -11,5 +21,25 @@ public class SearchServlet {
 	 */
 	public SearchServlet(ThreadSafeInvertedIndex invertedIndex) {
 		this.invertedIndex = invertedIndex;
+	}
+
+	@Override
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+		String query = request.getParameter("q");
+		query = query == null ? "" : StringEscapeUtils.escapeHtml4(query);
+
+		TreeSet<String> queryStems = FileStemmer.uniqueStems(query);
+		List<InvertedIndex.SearchResult> searchResults = this.invertedIndex.partialSearch(queryStems);
+
+		StringBuilder resultHtml = new StringBuilder();
+		resultHtml.append("<h1>Search Results</h1>");
+
+		for (var searchResult : searchResults) {
+			resultHtml.append(String.format("<p><a href=\"%s\">%s</a></p>", searchResult.getLocation(), searchResult.getLocation()));
+		}
+
+		response.setContentType("text/html");
+		response.setStatus(HttpServletResponse.SC_OK);
+		response.getWriter().println(resultHtml.toString());
 	}
 }
