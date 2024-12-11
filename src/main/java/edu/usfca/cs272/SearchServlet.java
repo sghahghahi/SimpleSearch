21c28be96b620774fd8638dd0c52244a7ab98b0f
sghahghahi <shyon.ghahghahi@gmail.com>
2024-12-11 00:09:07 -0800

    Add constructor to SearchServlet

--- src/main/java/edu/usfca/cs272/SearchServlet.java
+++ src/main/java/edu/usfca/cs272/SearchServlet.java
@@ -1,4 +1,15 @@
 package edu.usfca.cs272;
 
 /** TODO */
-public class SearchServlet {}
+public class SearchServlet {
+	/** TODO */
+	private final ThreadSafeInvertedIndex invertedIndex;
+
+	/**
+	 * TODO
+	 * @param invertedIndex TODO
+	 */
+	public SearchServlet(ThreadSafeInvertedIndex invertedIndex) {
+		this.invertedIndex = invertedIndex;
+	}
+}
